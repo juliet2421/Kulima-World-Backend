e431cf57da86334ed9f98caab670fa800e1ac5cd
Enigma2659 <bed-com-18-22@unima.ac.mw>
2024-11-30 03:54:42 -1100

    updated Strategy feaure

--- src/Strategy/loca-stratagy.mjs
+++ src/Strategy/loca-stratagy.mjs
@@ -1,8 +1,7 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
-import { User } from "../dbSchemers/userSchemer.mjs";
 import { comparePassword } from "../utils/helpers.mjs";
-
+import { User } from "../dbSchemers/userSchema.mjs";
 //serilization
 passport.serializeUser((user,done) => {
   done(null,user.id)
@@ -19,10 +18,10 @@ passport.deserializeUser(async(id,done)=>{
 })
 //local authetication
 export default passport.use(
-  new Strategy(async(username,password,done) => {
+  new Strategy({usernameField: "email"},async(email,password,done) => {
     try {
-        const findUser = await User.findOne({username});
-        if(!findUser) throw new Error( `Userr not found`);
+        const findUser = await User.findOne({email});
+        if(!findUser) throw new Error( `User not found`);
         if(!comparePassword(password,findUser.password)) throw Error(`Envalide Password`)
         done(null,findUser);
        
